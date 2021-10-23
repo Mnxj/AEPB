04d68e466430f27163327d65aec4a9fa81df57c3
shitianhang <773178360@qq.com>
2021-10-23 20:50:25 +0800

    [tianhang] #N/A feat:  add parkingBoy parking.

--- src/test/java/com/example/AEPB/ParkingBoyTest.java
+++ src/test/java/com/example/AEPB/ParkingBoyTest.java
@@ -1,5 +1,10 @@
 package com.example.AEPB;
 
+import com.example.parking.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /*
  * given 1号停车场的只有一个空车位，停车小弟。
  * when 停车
@@ -22,5 +27,28 @@ package com.example.AEPB;
  * then 取车成功
  */
 public class ParkingBoyTest {
+    private ParkingLot setupNumParkingLot(int num) {
+        ParkingLot parkingLot = new ParkingLot();
+        for (int i = 1; i <= num; i++) {
+            Car car = new Car();
+            parkingLot.parkingCarAndGetTicket(car);
+        }
+        return parkingLot;
+    }
+
+    @Test
+    void should_return_ticket_successfully_when_parkingBoy_parking_log_given_parkingLot_one_which_has_49_car_and_one_parking_car(){
+        //given
+        ParkingLot parkingLot = setupNumParkingLot(49);
+        ParkingLotGroup parkingLotGroup =new ParkingLotGroup();
+        parkingLotGroup.getParkingLotList().get(0).setParkingLotMap(parkingLot.getParkingLotMap());
+        ParkingBoy parkingBoy= new ParkingBoy();
+        Car car = new Car();
+        //when
+        Ticket ticket = parkingBoy.parkingBoyParkingCarAndGetTicket(parkingLotGroup, car);
+        //then
+        assertNotNull(ticket);
+
+    }
 
 }
