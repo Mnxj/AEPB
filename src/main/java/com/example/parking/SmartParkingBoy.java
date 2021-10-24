bd2c426564c4db8d55ba230908ce97d94a8bf4b0
shitianhang <773178360@qq.com>
2021-10-25 00:04:43 +0800

    [tianhang] #N/A feat:  add SmartParkingBoy parking.

---
+++ src/main/java/com/example/parking/SmartParkingBoy.java
@@ -0,0 +1,20 @@
+package com.example.parking;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SmartParkingBoy {
+    public Ticket smartParkingBoyParkingCarAndGetTicket(ParkingLotGroup parkingLotGroup, Car car) {
+        List<ParkingLot> parkingLotList = new ArrayList<>(parkingLotGroup.getParkingLotList());
+        parkingLotList.sort(Comparator.comparingInt(p -> p.getParkingLotMap().size()));
+        int parkingLotNo=0;
+        for (ParkingLot parkingLot:parkingLotGroup.getParkingLotList()){
+            if (parkingLot.equals(parkingLotList.get(0))){
+                return parkingLotGroup.parkingCarAndGetTicket(parkingLotNo, car);
+            }
+            parkingLotNo++;
+        }
+        return null;
+    }
+}
--- src/test/java/com/example/AEPB/SmartParkingBoyTest.java
+++ src/test/java/com/example/AEPB/SmartParkingBoyTest.java
@@ -5,6 +5,10 @@ package com.example.AEPB;
  * when 停车
  * then 停车成功并获得车票，停在了2号停车位
  *
+ * given 1号和2号停车场停有1辆车，其他停车场为空，smart Parking boy，一辆车
+ * when 停车
+ * then 停车成功并获得车票，停在了2号停车位
+ *
  * given 都是空停车场，smart boy，一辆车
  * when 停车
  * then  停车成功并获得车票，停在了1号停车位
@@ -23,7 +27,31 @@ package com.example.AEPB;
  *
 */
 
+import com.example.parking.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class SmartParkingBoyTest {
+    private ParkingLot setupNumParkingLot(int num) {
+        ParkingLot parkingLot = new ParkingLot();
+        for (int i = 1; i <= num; i++) {
+            Car car = new Car();
+            parkingLot.parkingCarAndGetTicket(car);
+        }
+        return parkingLot;
+    }
+    @Test
+    void should_return_ticket_successfully_when_smartParkingBoy_parking_given_parkingLot_one_which_has_49_and_empty_parkingLot_Two_and_one_parking_car(){
+        SmartParkingBoy smartParkingBoy=new SmartParkingBoy();
+        ParkingLotGroup parkingLotGroup=new ParkingLotGroup();
+        ParkingLot parkingLot = setupNumParkingLot(49);
+        parkingLotGroup.getParkingLotList().get(0).setParkingLotMap(parkingLot.getParkingLotMap());
+        Car car= new Car();
+        Ticket ticket = smartParkingBoy.smartParkingBoyParkingCarAndGetTicket(parkingLotGroup, car);
+        assertNotNull(ticket);
+        assertEquals(1,parkingLotGroup.getParkingLotList().get(1).getParkingLotMap().size());
+    }
+
 }
