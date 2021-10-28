83597e5db6455916e4fc339bab5e4942ef676035
shitianhang <773178360@qq.com>
2021-10-28 17:30:47 +0800

    [tianhang] #N/A feat:  add all tasks.

---
+++ src/test/java/com/example/AEPB/ParkingRobotTest.java
@@ -0,0 +1,34 @@
+package com.example.AEPB;
+
+public class ParkingRobotTest {
+    /*
+     * 1.
+     * given 两个停车场，1号停车场20个停车位停有1辆车，2号停车场40个停车位停有2两辆车，ParkingRobot，一辆车
+     * when 停车
+     * then 停车成功并获得车票，停在了2号停车位
+     * */
+    /*
+     * 2.
+     * given 两个个停车场，1号停车场1个停车位没有停车，2号停车场2个停车位没有停车，ParkingRobot，一辆车
+     * when 停车
+     * then 停车成功并获得车票，停在了1号停车位
+     * */
+    /*
+     * 3.
+     * given 两个停车场，1号停车场4个停车位停满，2号停车场5个停车位停满，ParkingRobot，一辆车
+     * when 停车
+     * then 停车失败并抛异常
+     * */
+    /*
+     * 4.
+     * given 一个停车场，停车场5个停车位没有车，ParkingRobot，一辆车
+     * when 停车
+     * then 停车成功并获得车票
+     * */
+    /*
+     * 5.
+     * given 一个停车场，停车场2个停车位和一辆车，ParkingRobot，对应的车票
+     * when 取车
+     * then 取车成功
+     * */
+}
